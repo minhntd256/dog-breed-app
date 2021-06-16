0bdd10da4de265b6a5b860c543628a7c931362de
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2021-06-16 20:32:33 +0700

    update information dog breed

--- data.py
+++ data.py
@@ -1,131 +1,131 @@
 description = [
     {
         'name': 'affenpinscher',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Borismindre.jpg/340px-Borismindre.jpg',
+        'weight': '2,9 – 6 kg (Trưởng thành)',
+        'height': '23 – 30 cm (Trưởng thành)',
+        'description': 'Một con chó Affenpinscher thường nặng 6,5 đến 13,2 pounds (2,9-6,0 kg) và cao từ 9 đến 12 inch (23 đến 30 cm) tính đến vai. Nó có một lớp lông thô ráp nếu không bị cắt tỉa, và nếu được cắt tỉa, lớp lông này có thể mềm hơn. Lớp lông của nó xù xì hơn trên đầu và vai tạo thành bờm, với lông ngắn hơn trên lưng. Lông của nó cứng và xù xì khi được cắt tỉa đúng cách.',
+        'origin': 'Đức, Pháp'
     },
     {
         'name': 'afghan_hound',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Afghan_Hound.jpg',
+        'weight': '20–27 kg (Trưởng thành)',
+        'height': '61–73 cm (Trưởng thành)',
+        'description': 'Chó săn Afghan có ngoại hình nhìn chung ấn tượng, đầu chúng ngẩng cao, trông kiêu hãnh. Chúng thường biểu lộ vẻ trầm ngâm là một đặc trưng của loài chó này. Chúng tạo một ấn tượng mạnh về sức mạnh, sự dẻo dai và vẻ đường bệ, kết hợp với tốc độ và sức mạnh. Giống chó này quan sát mọi thứ rất nhanh, ít khi nán lại để nhìn lâu. Trong chúng khá đường bệ và di biệt, đồng thời có một vẻ sắc sảo, mạnh mẽ. Chiều cao của chó đực 68–74 cm (27-29 ins) còn chó cái là 63–69 cm (25-27 ins).',
+        'origin': 'Afghanistan'
     },
     {
         'name': 'african_hunting_dog',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/The_Warrior_%2848031687366%29.jpg/600px-The_Warrior_%2848031687366%29.jpg',
+        'weight': '22 kg (Trưởng thành)',
+        'height': '92 cm (Trưởng thành)',
+        'description': 'Chó hoang dã châu Phi là một động vật xã hội cao, sống trong các bầy có phân cấp thống trị riêng biệt cho con đực và con cái. Duy nhất trong số động vật ăn thịt xã hội, con cái chứ không phải là con đực sinh sống phân tán khỏi đàn khi thành thục về sinh dục, và con non được ưu tiên cho ăn đầu tiên khi bắt được con mồi. Loài này chuyên săn linh dương, bắt bằng cách đuổi theo chúng để làm con mồi mệt lữ và kiệt sức.',
+        'origin': 'Châu Phi'
     },
     {
         'name': 'airedale',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/5/52/Airedale_Terrier.jpg',
+        'weight': '40–50 pound (18–23 kg)',
+        'height': '22–24 inch (56–61 cm)',
+        'description': 'Giống như nhiều loài chó săn, giống chó này có lớp lông “gãy”, rất cứng và nhiều lông. Điều này có nghĩa là lông của loài này không quá dài để thành những đám rối, lông thẳng và gần nhau, bao bọc cơ thể và chân. Bộ lông ngoài cứng, nhiều lông, lớp lông phủ bên trong mềm hơn. Những bộ lông cứng nhất sẽ bị nhăn nheo hoặc hơi xoăn. Bộ lông mềm xoăn rất không được ưa thích.',
+        'origin': 'Vương quốc Anh'
     },
     {
         'name': 'american_staffordshire_terrier',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/6/66/AMERICAN_STAFFORDSHIRE_TERRIER%2C_Zican%E2%80%99s_Bz_Ez_Dragon_%2824208348891%29.2.jpg',
+        'weight': '25–32 kg',
+        'height': '46–48 cm',
+        'description': 'American Staffordshire Terrier là một giống chó trong nhóm chó pit bull được lai tạo với giống chó sục có nguồn gốc từ Hoa Kỳ. Là giống chó lông ngắn, có trọng lượng trung bình. Với thân hình nhìn rất lực lưỡng với nhiều cơ bắp phù hợp với tính cách chiến đấu.',
+        'origin': 'Hoa Kỳ'
     },
     {
         'name': 'appenzeller',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Appenzeller001.jpg/600px-Appenzeller001.jpg',
+        'weight': '22 – 32 kg (Trưởng thành)',
+        'height': '52–56 cm (Trưởng thành)',
+        'description': 'Chó Appenzeller là một giống chó chăn gia súc có nguồn gốc từ Thụy Sĩ. Đây là một trong 4 giống chó tam thể của Thụy Sĩ. Chúng thường được sử dụng làm chó canh gác, bảo vệ, kéo xe, chó trong nhà và các nông trại. Ngày nay, chúng được sử dụng làm chó nuôi trong gia đình và dùng để làm nhiều công việc khác nhau.',
+        'origin': 'Thụy Sĩ'
     },
     {
         'name': 'australian_terrier',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Australian_Terrier2.jpg/600px-Australian_Terrier2.jpg',
+        'weight': '6,4–7,3 kg',
+        'height': '23–28 cm',
+        'description': 'Chó sục Úc là một giống chó nhỏ, chân ngắn, cứng cáp. Chiều cao từ 23 đến 28 cm, nặng 4 đến 6 kg. Nó có đầu thủ dài, đôi tai dựng đứng, hình chữ V và cặp mắt đen lóe sáng. Mũi đen mà tạo thành hình chữ V giữa hai cánh mũi. Hai hàm răng nên khít như lưỡi kéo cắt. Cơ thể hơi dài hơn chiều cao, với lưng thẳng. Ngực khỏe khoắn, tương đối rộng và sâu.',
+        'origin': 'Úc'
     },
     {
         'name': 'basenji',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Asia_figlia_di_Dulcinea_%26_Daiquiri.jpg/600px-Asia_figlia_di_Dulcinea_%26_Daiquiri.jpg',
+        'weight': '20-25 pounds (9–11 kg.)',
+        'height': '16-17 inches (41–43 cm.)',
+        'description': 'Đây là giống chó săn có nguồn gốc từ lâu đời. Dấu hiệu đầu tiên của giống chó tương tự Basenji được tìm thấy ở những ngôi mộ ở Ai cập, và những bức vẽ trên tường từ 5000 năm trước. Còn được gọi là chó Congo, được mang đến châu Âu từ năm 1934. Những nhà nhân giống ở Anh đã chọn lọc và du nhập chúng ra toàn thế giới. Ở châu phi, Basenji được dùng để chỉ đường trong những khu rừng, để cảnh báo và đề phòng những con thú nguy hiểm tiến lại gần, và chúng còn được sử dụng để đi săn theo bầy đàn. Tộc dân Pig-mê (Azande và Mangbetu) gọi giống chó này là Vật nhỏ trong bụi và cho đến ngày nay chúng vẫn phục vụ cho việc săn bắn trên một vùng rộng lớn từ vùng trũng Kongo đến Sudan.',
+        'origin': 'Cộng hòa Dân chủ Congo'
     },
     {
         'name': 'basset',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Basset_hound_portrait.jpg',
+        'weight': '55–75 pound (25–34 kg)',
+        'height': '12–15 inch (30–38 cm)',
+        'description': 'Chó săn Basset là giống chó săn với bản tính tự nhiên và hiền hòa, chó săn Basset xuất hiện lần đầu vào thế kỉ 16, với mục đích săn bắt những loài thú nhỏ như con lửng hay thỏ. Tính cách không hiếu chiến của chó săn Basset khiến chúng được yêu thích bởi những người sành nuôi chó, những thợ săn và trong cả các gia đình. Năm 1585 taị Pháp, lần đầu tiên các tên chó Basset (chó săn chân lùn) xuất hiện trong một cuốn sách nói về chuyện đi săn và được chú ý tại Anh quốc vào những năm cuối của thập kỷ 1800. Tại đảo quốc này chúng được nuôi để săn thỏ.',
+        'origin': 'Anh, Pháp'
     },
     {
         'name': 'beagle',
         'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'weight': '22–25 lb (10–11 kg)',
+        'height': '13-16 in (33-41 cm)',
+        'description': 'Đây là một giống chó săn khá phổ biến, có đôi chút giống chó săn chân lùn (Basset hound) cả trong sự xuất hiện cũng như trong tính khí. Nguồn gốc của nó chưa được rõ ràng, cho dù chúng đã được nuôi tại Anh quốc trên 500 năm. Giống chó này có thể bắt nguồn từ lai tạo giữa giống Harrier và giống chó săn Anh khác. Beagle thường dùng để đi săn theo đàn, cũng có thể dùng một, hoặc một cặp chó để săn thỏ rừng, gà lôi và chim cút. Nó cũng được dùng như một loài chó phát hiện ma tuý tốt và là người tốt của gia đình. Do chúng khá đồng nhất trong kích thước, ở một kích thước nhỏ bé vừa phải, Beagle thường được dùng để thử nghiệm thuốc. Chúng được phân loại là chó săn loại nhỏ. Giống chó Beagle đã chứng tỏ khả năng thích ứng rất cao. Tại Anh quốc, chúng được sử dụng để săn thỏ, trong khi đó, tại Sri Lanka, chúng được cho săn heo rừng.',
+        'origin': '	Anh'
     },
     {
         'name': 'bedlington_terrier',
-        'img': '',
-        'weight': '',
-        'height': '',
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Diego_juillet2009_014.JPG/600px-Diego_juillet2009_014.JPG',
+        'weight': '18-23 cân Anh (8–10 kg)',
+        'height': '16-17 inches (41–43 cm)',
         'description': '',
-        'origin': ''
+        'origin': 'Anh'
     },
     {
         'name': 'bernese_mountain_dog',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Berne%C5%84ski_pies_pasterski_pl.jpg/600px-Berne%C5%84ski_pies_pasterski_pl.jpg',
+        'weight': '85–125 pound (39–57 kg)',
+        'height': '25–27,5 inch (64–70 cm)',
+        'description': 'Những con chó núi Bernese là hậu duệ của chó sói được thuần hóa để nuôi trong các trang trại ngày xưa với mục đích bảo vệ và kéo xe. Các loại ban đầu được gọi là Godly, được lại tạo ở một thị trấn nhỏ tên là Dürrbach, nơi những loại chó lớn thường được nuôi thường xuyên. Ở Harrisburg, người ta sử dụng những con chó như là công cụ chiến tranh.',
+        'origin': 'Thụy Sĩ'
     },
     {
         'name': 'black-and-tan_coonhound',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Black_and_Tan_Coonhound.jpg/600px-Black_and_Tan_Coonhound.jpg',
+        'weight': '58–68 cm',
+        'height': '45–64 kg',
+        'description': 'Black and Tan Coonhounds được tạo ra bằng cách lai tạo giữa loài chó đánh hơi và loài chó săn cáo Mỹ, chúng cơ bản là có màu đen và nâu. Nổi tiếng nhất là một loài chó săn lửng, Black and Tan Coonhounds sủa, tru khi trèo cây để săn thú. Loài chó này cũng được sử dụng một cách thành công để săn những loài thú khác như săn gấu, săn hươu, nai, sư tử núi, chồn opossum kể cả lúc mưa gió bão. Nó chịu đựng được thời tiết khắc nghiệt của mùa đông cũng như cái nắng gắt của mùa hè. Nhút nhát hoặc lo lắng có thể là không đảm bảo tiêu chuẩn đối với loài chó này. Được công nhận chính thức từ năm 1945. Ưu điểm của Black and Tan Coonhound bao gồm săn thú, truy tìm, canh gác và khéo léo.',
+        'origin': 'Hoa Kỳ'
     },
     {
         'name': 'blenheim_spaniel',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Kingcharlesspaniel.jpg/600px-Kingcharlesspaniel.jpg',
+        'weight': '13–18 lb (5,9–8,2 kg)',
+        'height': '12–13 inch (30–33 cm)',
+        'description': 'Tên của loài chó này được đặt bởi Vua Charles Đệ Nhị. Cavalier King Charles Spaniels là hậu duệ từ loài King Charles Spaniel. Những năm sau năm 1600, King Charles Spaniel được lai tạo với những con chó Pug, mà dẫn đến kết quả là tạo ra một giống chó nhỏ hơn với chiếc mũi phẳng hơn, khuôn mặt hãy hơn, đầu thủ tròn trịa và cặp mắt lồi. Kết quả của việc nhân giống này là giống chó King Charles Spaniel (English Toy Spaniel) được biết đến ngày nay.',
+        'origin': 'Anh'
     },
     {
         'name': 'bloodhound',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/c/c6/Bloodhound_800.jpg',
+        'weight': '33-50 kg',
+        'height': '64–69 cm',
+        'description': 'Theo một truyền thuyết, chúng được tạo ra vào 1000 năm Sau Công Nguyên bởi các tu sĩ tại St Hubert Monastery ở Bỉ và xuất xứ của nó là ở Pháp, quê hương của nhiều loài chó săn hiện đại. Những kỹ năng đánh hơi của nó được tạo ra khi nhân giống các loài chó săn thính hơi khác nhau, như loài Chó săn cáo Anh Quốc (English Foxhound), Chó săn cáo Mỹ (American Foxhound), Chó săn Gấu trúc (Coonhound), Chó săn Jura Thụy Sĩ, Chó săn miền núi Bavarian, và nhiều giống chó khác. Trong suốt những năm cuối thế kỷ 19, những con Bloodhound thường xuyên trở thành mẫu vẽ cho các họa sĩ như Edwin Landseer và Briton Riviere. Những con chó được khắc hoạ gần giống như hình dáng của những con Bloodhound hiện đại, ngụ ý rằng đặc tính cần thiết của loài Bloodhound là tiền đề cho việc tạo ra những con Bloodhound hiện đại. Tuy nhiên, những con chó được khắc hoạ bởi Lanseer có ít những nếp nhăn và ồn ào hơn những con Bloodhound hiện đại.',
+        'origin': 'Bỉ, Pháp, Vương Quốc Anh, Anh, Scôtlen'
     },
     {
         'name': 'bluetick',
-        'img': '',
-        'weight': '',
-        'height': '',
-        'description': '',
-        'origin': ''
+        'img': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/BluetickCoonhound.jpg/440px-BluetickCoonhound.jpg',
+        'weight': '55–80 lb (25–36 kg)',
+        'height': '22–27 in (56–69 cm)',
+        'description': 'Giống chó này có những dấu tích trên những đề đầi cổ do người Anh xây đựng tại chính quốc Anh và tại Mĩ khi mà những người anh di cư sang.Nó đã và đang là giống chó săn gấu trúc và thú có túi hàng đầu hiện nay. Chúng thích ứng với mọi môi trường săn bắt không tha cho bất kì con mồi nào mà chúng đã bắt gặp. Coonhound là giống chó kết hợp của sự bền bỉ và khứu giác tốt. Chúng thực sự nổi trội trong những khu rừng, vùng nước có những con mồi mà không mong chờ chủ đến.',
+        'origin': 'Hoa Kỳ'
     },
     {
         'name': 'border_collie',
